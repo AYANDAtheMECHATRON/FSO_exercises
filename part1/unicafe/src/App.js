8bde011de04dc125ae0d4072a6e51effef01778e
AYANDAtheMECHATRON <64664640+AYANDAtheMECHATRON@users.noreply.github.com>
2023-07-14 23:19:27 +0200

    exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,16 +4,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] =useState(0)
+  const [average, setAverage] =useState(0)
+  const [positive, setPositive] = useState(0)
+
 
   const handleGood =()=>{
-   setGood(good+1)
+    const updatedGood = good+1
+   setGood(updatedGood)
+   console.log('good', updatedGood)
+   setTotal(updatedGood+bad + neutral)
+   const updatedTotal = updatedGood+bad+neutral
+   const pos = (updatedGood/updatedTotal)
+   setPositive(pos)
+   const av = ((updatedGood-bad)/updatedTotal)
+   setAverage(av)
   }
 
   const handleNeutral =() =>{
-    setNeutral(neutral+1)
+    const updatedNeutral = neutral+1
+    setNeutral(updatedNeutral)
+    setTotal(updatedNeutral + good + bad)
+    const updatedTotal =updatedNeutral + good + bad
+    const pos = (good/updatedTotal)
+    setPositive(pos)
   }
   const handleBad =()=>{
-    setBad(bad+1)
+    const updatedBad = bad +1
+    setBad(updatedBad)
+    setTotal(updatedBad+good + neutral)
+    const updatedTotal =updatedBad + good + neutral
+    const pos = (good/updatedTotal)
+    setPositive(pos)
+    const av = (good-updatedBad)/updatedTotal
+    setAverage(av)
   }
 
   return(
@@ -28,6 +52,10 @@ const App = () => {
     <p>good {good}</p>
     <p>neutral {neutral}</p>
     <p>bad {bad}</p>
+    <p>all {total}</p>
+    <p>average {average}</p>
+    <p>positive {positive*100} %</p>
+    
  </div>
   )
 }
