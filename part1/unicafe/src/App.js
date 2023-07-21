838437300b81fb1ef9481bcac79064020a1c7a74
AYANDAtheMECHATRON <64664640+AYANDAtheMECHATRON@users.noreply.github.com>
2023-07-21 17:32:58 +0200

    exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,48 +1,44 @@
 import {useState} from 'react'
 
+let positiveAvg = 0;
+let average = 0;
+let total = 0;
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] =useState(0)
-  const [average, setAverage] =useState(0)
-  const [positive, setPositive] = useState(0)
+
 
 
   const handleGood =()=>{
     const updatedGood = good+1
    setGood(updatedGood)
-   console.log('good', updatedGood)
-   setTotal(updatedGood+bad + neutral)
-   const updatedTotal = updatedGood+bad+neutral
-   const pos = (updatedGood/updatedTotal)
-   setPositive(pos)
-   const av = ((updatedGood-bad)/updatedTotal)
-   setAverage(av)
+   total =updatedGood+bad + neutral
+   positiveAvg = (updatedGood/total)
+   average = ((updatedGood-bad)/total)
+ 
+   
   }
 
   const handleNeutral =() =>{
     const updatedNeutral = neutral+1
     setNeutral(updatedNeutral)
-    setTotal(updatedNeutral + good + bad)
-    const updatedTotal =updatedNeutral + good + bad
-    const pos = (good/updatedTotal)
-    setPositive(pos)
+    total =(updatedNeutral + good + bad)
+    positiveAvg = (good/total)
+
+  
   }
   const handleBad =()=>{
     const updatedBad = bad +1
     setBad(updatedBad)
-    setTotal(updatedBad+good + neutral)
-    const updatedTotal =updatedBad + good + neutral
-    const pos = (good/updatedTotal)
-    setPositive(pos)
-    const av = (good-updatedBad)/updatedTotal
-    setAverage(av)
+    total= updatedBad+good + neutral
+    positiveAvg = (good/total)
+    average = (good-updatedBad)/total
   }
-
+  console.log('tot', total)
   return(
   <div>
-    <div><h2>give feedback</h2></div>
     <div>
     <button onClick={handleGood}>good</button>
     <button onClick={handleNeutral}>neutral</button>
@@ -54,7 +50,7 @@ const App = () => {
     <Statistics text ='bad' value = {bad}/>
     <Statistics text ='all' value = {total}/>
     <Statistics text ='average' value = {average}/>
-    <Statistics text ='positive' value = {positive*100}/>
+    <Statistics text ='positive' value = {positiveAvg*100}/>
 
     
  </div>
@@ -62,6 +58,11 @@ const App = () => {
 }
 
 const Statistics = ({text,value}) =>{
+if(total === 0 && average === 0 && positiveAvg ===0){
+   return(
+    <h3>give feedback</h3>
+   )
+}
   return(
     <div>
       <p>{text} {value}</p>
